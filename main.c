7fbb98f9f8c7b845f94830db54a1265f99417a68
TheDevMode <tmode277@gmail.com>
2026-09-12 12:08:49 +0000

    done

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-    int a, b;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    printf("%d\n", a * b);
-    
+    int s[5];
+    for (int i = 0; i < 5; i++) {
+        scanf("%d",&s[i]);
+    }
+
+    int best = s[0];
+    for (int i = 1; i < 5; i++) {
+        if (s[i] > best) {
+            best = s[i];
+        }
+    }
+    printf("%d\n", best);
 
     return 0;
-}
+    }
